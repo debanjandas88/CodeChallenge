d1ca8e79a5f2cd576953a3794305095b441d4eea
debanjandas88 <109985599+debanjandas88@users.noreply.github.com>
2022-07-30 19:41:31 +0530

    Update Deploy-Three-Tier-Architecture-Main.ps1

--- Codes/Deploy-3-Tier-Application-Architecture/Deploy-Three-Tier-Architecture-Main.ps1
+++ Codes/Deploy-3-Tier-Application-Architecture/Deploy-Three-Tier-Architecture-Main.ps1
@@ -9,6 +9,192 @@ This Script is used to Deploy 3-Tier architecture in a modularized fasion
 
 .\ScriptToFetchMetadataOfAzureResources.ps1 -ResourceName 'myVM'
 
+.PARAMETER ResourceGroupName
+  Specifies the resource group name
+
+.PARAMETER Location
+ Specifies the location
+
+.PARAMETER SubscriptionName
+ Specifies the Subscription name
+
+.PARAMETER ApplicationID
+ Specifies the ApplicationID for tagging purpose
+
+.PARAMETER ApplicationName
+ Specifies the Application name for tagging purpose
+
+.PARAMETER AppgatewayName
+  Specifies the application gateway name
+
+.PARAMETER AppGatewaySkuName
+  Specifies the application gateway SKU 
+
+.PARAMETER AppGatewayTier
+  Specifies the application gateway tier 
+
+.PARAMETER AGSubnetName
+  Specifies the application gateway subnet name
+
+.PARAMETER AGAddressPrefix
+  Specifies the application gateway subnet address prefix
+
+.PARAMETER BackEndsubnetName
+  Specifies the application gateway backend subnet name
+
+.PARAMETER BackEndAddressPrefix
+  Specifies the application gateway backend subnet address prefix
+
+.PARAMETER VnetName
+  Specifies the application gateway virtual network name
+
+.PARAMETER VnetAddressPrefix
+  Specifies the application gateway virtual network address prefix
+
+.PARAMETER PIPName
+  Specifies the application gateway public ip name
+
+.PARAMETER Keyvault2
+  Specifies the keyvault for storing windows and sql server admin credentials
+
+.PARAMETER AppGatewayIPName
+  Specifies the application gateway gateway IP name
+
+.PARAMETER AGFrontendIPConfigName
+  Specifies the application gateway frontend IP configuration name
+
+.PARAMETER AGFrontEndPortName
+  Specifies the application gateway frontend port name
+
+.PARAMETER AGFrontendIPConfigName
+  Specifies the application gateway frontend IP configuration name
+
+.PARAMETER AGFrontEndPortName
+  Specifies the application gateway frontend port name
+
+.PARAMETER AGBackEndPoolName
+  Specifies the application gateway backend pool name
+
+.PARAMETER ListenerName
+  Specifies the application gateway listener name
+
+.PARAMETER KeyVaultName
+  Specifies the keyvault name for certificate mgmt if any
+
+ .PARAMETER FrontEndRuleName
+ 
+.PARAMETER LAWorkSpaceName
+  Specifies the log analytics workspace name for monitoring
+
+.PARAMETER LASku
+  Specifies the log analytics workspace sku
+
+.PARAMETER DNSName
+  Specifies the domain name server used for app gateway health probe
+
+.PARAMETER HealthProbeName
+  Specifies the health probe name of app gateway
+
+.PARAMETER AGBackEndPoolSettingName
+  Specifies the app gateway backend pool settings name
+
+.PARAMETER FrontEndRuleName
+  Specifies the app gateway front end rule name
+
+.PARAMETER VMName1, VMName2, VMName3, VMName4
+  Specifies the VM names to be deployed 
+
+.PARAMETER Nicname1, Nicname2, Nicname3, Nicname14
+  Specifies the netwrok interface card names to be used for the above VM deployments
+
+.PARAMETER Location
+ Specifies the location of the resources
+ 
+.PARAMETER VMSize
+  Specifies the VMSize
+
+.PARAMETER VMSku
+  Specifies the VMSku
+
+.PARAMETER SQLServerName
+ Specifies the SQLServerName
+
+.PARAMETER SQLDatabaseName
+  Specifies the SQLDatabaseName
+
+.PARAMETER RequestedServiceObjectiveName
+  Specifies the service tier of Azure SQL database
+
+.PARAMETER ILBName
+  Specifies the Load balancer name
+
+.PARAMETER ILBName
+  Specifies the load balancer SKU 
+
+.PARAMETER NatGatewayIPName
+  Specifies the NAT gateway IP name
+
+.PARAMETER NATGatewayName
+  Specifies the NAT gateway  name
+
+.PARAMETER IdleTimeOutMin
+  Specifies the idle timout of nat gateway and load balancer 
+
+.PARAMETER LBBackendSubnet
+  Specifies the ILB backend subnet name
+
+.PARAMETER LBBackendSubnetAddressPrefix
+  Specifies the ILB backend subnet address prefix
+
+.PARAMETER LBBastionSubName
+  Specifies the ILB bastion subnet name
+
+.PARAMETER LBbastionhostname
+  Specifies the ILB bastion host name
+
+.PARAMETER LBBastionSubAddressPrefix
+  Specifies the ILB bastion subnet address prefix
+
+.PARAMETER LBbastionPIP
+  Specifies the ILB bastion public IP address
+
+.PARAMETER FrontEndName
+  Specifies the ILB front end name
+
+.PARAMETER PrivateIPAddress
+  Specifies the ILB front end private IP address
+
+.PARAMETER LBBackEndPoolName
+  Specifies the ILB backend pool name
+
+.PARAMETER LBHeathProbeName
+  Specifies the ILB health probe name
+
+ .PARAMETER LBHealthProbeProtocol
+  Specifies the ILB health probe protocol
+
+.PARAMETER LBHealthProbePort
+  Specifies the ILB health probe port
+
+.PARAMETER LBRuleName
+  Specifies the load balancer rule name
+
+.PARAMETER LBProtocol
+  Specifies the load balancer rule's protocol
+
+.PARAMETER LBFrontEndPort
+  Specifies the load balancer front end port
+
+.PARAMETER LBBackEndPort
+  Specifies the load balancer back end port
+
+.PARAMETER NSGName
+  Specifies the NSG name
+
+.PARAMETER RSVName
+ Specifies the recovery service vault name
+
+
 .Notes
 The resource creation is performed by module function so as to make the structure modularized.
 Module function can be accessed in the following path
@@ -45,6 +231,8 @@ param(
     [string]$VnetName,
     [Parameter(Mandatory = $true)]
     [string]$VnetAddressPrefix,
+    [Parameter(Mandatory = $true)]
+    [string]$KeyVaultName2,
     [Parameter(Mandatory = $true)]
     [string]$PIPName,
     [Parameter(Mandatory = $true)]
@@ -95,8 +283,8 @@ param(
     [string]$RequestedServiceObjectiveName,
     [Parameter(Mandatory = $true)]
     [string]$ILBName,
-    [Parameter(Mandatory = $false)]
-    [string]$Sku,
+    [Parameter(Mandatory = $true)]
+    [string]$LBSku,
     [Parameter(Mandatory = $false)]
     [string]$NatGatewayIPName,
     [Parameter(Mandatory = $false)]
@@ -107,38 +295,14 @@ param(
     [string]$LBBackendSubnet,
     [Parameter(Mandatory = $true)]
     [string]$LBBackendSubnetAddressPrefix,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string]$LBBastionSubName,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string]$LBBastionSubAddressPrefix,
-    [Parameter(Mandatory = $true)]
-    [string]$LBVnetName,
-    [Parameter(Mandatory = $true)]
-    [string]$LBVnetAddressPrefix,
     [Parameter(Mandatory = $false)]
     [string]$LBbastionhostname,
     [Parameter(Mandatory = $false)]
     [string]$LBbastionPIP,
-    [Parameter(Mandatory = $false)]
-    [string]$LBRuleName,
-    [Parameter(Mandatory = $true)]
-    [string]$LBRuleDescription,
-    [Parameter(Mandatory = $true)]
-    [string]$LBProtocol,
-    [Parameter(Mandatory = $true)]
-    [string]$LBSourcePortRange,
-    [Parameter(Mandatory = $true)]
-    [string]$LBDestinationPortRange,
-    [Parameter(Mandatory = $true)]
-    [string]$LBSourceAddressPrefix,
-    [Parameter(Mandatory = $true)]
-    [string]$LBDestinationAddressPrefix,
-    [Parameter(Mandatory = $false)]
-    [string]$LBAccess,
-    [Parameter(Mandatory = $false)]
-    [string]$LBPriority,
-    [Parameter(Mandatory = $false)]
-    [string]$LBDirection,
     [Parameter(Mandatory = $true)]
     [string]$FrontEndName,
     [Parameter(Mandatory = $true)]
@@ -152,27 +316,21 @@ param(
     [Parameter(Mandatory = $true)]
     [string]$LBHealthProbePort,
     [Parameter(Mandatory = $true)]
-    [string]$LBRuleName2,
+    [string]$LBRuleName,
     [Parameter(Mandatory = $true)]
-    [string]$LBProtocol2,
+    [string]$LBProtocol,
     [Parameter(Mandatory = $true)]
     [string]$LBFrontEndPort,
     [Parameter(Mandatory = $true)]
     [string]$LBBackEndPort,
     [Parameter(Mandatory = $false)]
-    [string]$NSGName,
-    [Parameter(Mandatory = $true)]
-    [string]$NWRuleName,
-    [Parameter(Mandatory = $true)]
-    [string]$NWRuleDescription,
-    [Parameter(Mandatory = $true)]
-    [string]$NWProtocol,
+    [string]$NSGName,   
     [Parameter(Mandatory = $true)]
     [string]$RSVName,
+    [Parameter(Mandatory = $true)]
+    [string]$DNSName,
     [Parameter(Mandatory = $false)]
-    [string]$SourcePortRange,
-    [Parameter(Mandatory = $false)]
-    [string]$DestinationPortRange
+    [string]$HealthProbeName
 )
 
 try {
@@ -185,6 +343,12 @@ try {
     Write-Host "subscription context set"
 
     #endRegion
+
+    ## importing the required modules
+    $commonModulePath = Join-Path $PSScriptRoot -ChildPath 'Modules\CommonModules'
+
+    Import-Module -name $commonModulePath -Verbose
+    Write-Host "Imported required modules"
 
     #region Create Resource Group if not existing
 
@@ -242,6 +406,8 @@ try {
             LASku                    = $LASku
             TaggingData              = $TaggingData
             SubscriptionID           = $SubscriptionID
+            HealthProbeName          = $HealthProbeName
+            DNSName                  = $DNSName
         }
     
         New-AzureAppGateway @createAppGatewayParams  
@@ -251,6 +417,7 @@ try {
     }
     #endRegion
 
+    
     #region Create web tier virtual machines to be added as the backend og application gateway
 
     #region fetching the backend pool information
@@ -273,8 +440,8 @@ try {
     $nicname += ($nicName1, $nicName2)
     Write-Host "Nic Names names are $nicname"
 
-    $adminUser = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name 'adminusername' -AsPlainText
-    $adminPAss = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name 'adminpassword' -AsPlainText
+    $adminUser = Get-AzKeyVaultSecret -VaultName $KeyVaultName2 -Name 'adminusername' -AsPlainText
+    $adminPAss = Get-AzKeyVaultSecret -VaultName $KeyVaultName2 -Name 'adminpassword' -AsPlainText
     $secureString = ConvertTo-SecureString -String $adminPAss -AsPlainText -Force
     $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $adminUser, $secureString
 
@@ -292,7 +459,6 @@ try {
             $createVMParams = @{
                 ResourceGroupName = $ResourceGroupName
                 SubscriptionId    = $SubscriptionId
-                AVSetName         = $AVSetName
                 VMName            = $VMName[$i - 1]
                 VMSize            = $VMSize
                 Location          = $Location
@@ -312,6 +478,17 @@ try {
 
     #endRegion
 
+    #region restarting the application gateway
+    Write-Host "Stopping App gateway"
+    Stop-AzApplicationGateway -ApplicationGateway $appgw
+ 
+    # Start the Azure Application Gateway
+    Write-Host "Starting App gateway"
+    Start-AzApplicationGateway -ApplicationGateway $appgw
+    Write-Host "App gateway restarted"
+
+    #endRegion
+
     #region Create Internal load balancer for the business tier
 
     Write-Host "Create Internal load balancer for the business tier"
@@ -320,11 +497,20 @@ try {
     if ($null -eq $checkILB) {
         Write-Host "The load balancer of name $ILBName does not exist. Calling the function for creating the same."
 
+        ##get the subnet  address prefix  of app gw backend
+
+        $vnet   = Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $vnetName
+        $apgwbesubnet = $vnet.Subnets[1].AddressPrefix
+
+        $LBRuleName = $AGBackEndPoolName + '-' + $ILBName + '-Connect'
+
+        Write-Host "Front end subnet of app gw : $apgwfesubnet"
+
         $createILBParams = @{
             ResourceGroupName            = $ResourceGroupName
             ILBName                      = $ILBName
             Location                     = $Location
-            Sku                          = $Sku
+            Sku                          = $LBSku
             NatGatewayIPName             = $NatGatewayIPName
             NaTGatewayName               = $NaTGatewayName
             IdleTimeOutMin               = $IdleTimeOutMin
@@ -332,28 +518,28 @@ try {
             LBBackendSubnetAddressPrefix = $LBBackendSubnetAddressPrefix
             LBBastionSubName             = $LBBastionSubName
             LBBastionSubAddressPrefix    = $LBBastionSubAddressPrefix
-            LBVnetName                   = $LBVnetName
-            LBVnetAddressPrefix          = $LBVnetAddressPrefix
+            LBVnetName                   = $VnetName
+            LBVnetAddressPrefix          = $VnetAddressPrefix
             LBbastionhostname            = $LBbastionhostname
             LBbastionPIP                 = $LBbastionPIP
             LBRuleName                   = $LBRuleName
-            LBRuleDescription            = $LBRuleDescription
-            LBProtocol                   = $LBProtocol
-            LBSourcePortRange            = $LBSourcePortRange
-            LBDestinationPortRange       = $LBDestinationPortRange
-            LBSourceAddressPrefix        = $LBSourceAddressPrefix
-            LBDestinationAddressPrefix   = $LBDestinationAddressPrefix
-            LBAccess                     = $LBAccess
-            LBPriority                   = $LBPriority
-            LBDirection                  = $LBDirection
+            LBRuleDescription            = "Connect web tier to business tier"
+            LBProtocol                   = "tcp"
+            LBSourcePortRange            = "443"
+            LBDestinationPortRange       = "443"
+            LBSourceAddressPrefix        = $apgwbesubnet
+            LBDestinationAddressPrefix   = $PrivateIPAddress
+            LBAccess                     = "Allow"
+            LBPriority                   = "125"
+            LBDirection                  = "Inbound"
             FrontEndName                 = $FrontEndName
             PrivateIPAddress             = $PrivateIPAddress
             LBBackEndPoolName            = $LBBackEndPoolName
             LBHeathProbeName             = $LBHeathProbeName
             LBHealthProbeProtocol        = $LBHealthProbeProtocol
             LBHealthProbePort            = $LBHealthProbePort
-            LBRuleName2                  = $LBRuleName2
-            LBProtocol2                  = $LBProtocol2
+            LBRuleName2                  = $LBRuleName
+            LBProtocol2                  = $LBProtocol
             LBFrontEndPort               = $LBFrontEndPort
             NSGName                      = $NSGName
             SubscriptionId               = $subscriptionId
@@ -390,26 +576,25 @@ try {
 
     Write-Host "Nic names are $nicnameLB"
 
-    $adminUser2 = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name 'adminusername' -AsPlainText
-    $adminPAss2 = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name 'adminpassword' -AsPlainText
+    $adminUser2 = Get-AzKeyVaultSecret -VaultName $KeyVaultName2 -Name 'adminusername' -AsPlainText
+    $adminPAss2 = Get-AzKeyVaultSecret -VaultName $KeyVaultName2 -Name 'adminpassword' -AsPlainText
     $secureString2 = ConvertTo-SecureString -String $adminPAss2 -AsPlainText -Force
     $Credential2 = New-Object System.Management.Automation.PSCredential -ArgumentList $adminUser2, $secureString2
 
     #endRegion
 
-    #region create backedn VMS ILB
-
-    for ($i = 1; $i -le 1; $i++) {
+    #region create backend VMs of ILB
+    $vnet = Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $vnetName
+    for ($i = 1; $i -le 2; $i++) {
 
         Write-Host "Creating NIC $($nicNameLB[$i-1]) for VM name  $($VMNameLB[$i-1])"
-        $nic = New-AzNetworkInterface -Name $nicNameLB[$i - 1] -ResourceGroupName $ResourceGroupName -Location $Location -Subnet $vnet.Subnets[2] -NetworkSecurityGroup  $fetchNSG -LoadBalancerBackendAddressPool $getILBbePool
+        $nic = New-AzNetworkInterface -Name $nicNameLB[$i - 1] -ResourceGroupName $ResourceGroupName -Location $Location -Subnet $vnet.Subnets[2] -NetworkSecurityGroup  $fetchNSG -LoadBalancerBackendAddressPool $getILBbePool 
         $checkVMExitenceLB = Get-AzVM -Name $VMNameLB[$i - 1] -ErrorAction SilentlyContinue
         if ($null -eq $checkVMExitenceLB) {
             Write-Host "Calling function to create VM name  $($VMNameLB[$i-1])"
             $createVMParams = @{
                 ResourceGroupName = $ResourceGroupName
                 SubscriptionId    = $SubscriptionId
-                AVSetName         = $AVSetName
                 VMName            = $VMNameLB[$i - 1]
                 VMSize            = $VMSize
                 Location          = $Location
@@ -441,8 +626,8 @@ try {
     $nicDetails02 = Get-AzNetworkInterface -ResourceGroupName $ResourceGroupName -Name $nicName4
     $privateIPAddress02 = $nicDetails02.IpConfigurations[0].PrivateIpAddress
 
-    $privateIPAddress = @()
-    $privateIPAddress += ($privateIPAddress01, $privateIPAddress02)
+    $privateIPAddresses = @()
+    $privateIPAddresses += ($privateIPAddress01, $privateIPAddress02)
 
     Write-Host "The private IP address of the business tier VMs are: $privateIPAddress"
 
@@ -456,45 +641,13 @@ try {
         SQLServerName                 = $SQLServerName
         SQLDatabaseName               = $SQLDatabaseName
         SubscriptionId                = $SubscriptionId
-        KeyVaultName                  = $KeyVaultName
+        KeyVaultName                  = $KeyVaultName2
         RequestedServiceObjectiveName = $RequestedServiceObjectiveName
-        AllowedIPs                    = $privateIPAddress
+        AllowedIPs                    = $privateIPAddresses
         TaggingData                   = $taggingData
     }
 
     New-SQLDatabase @createSQLServerParams
-
-    #endRegion
-
-    #region create network rule from web to business tier
-
-
-    Write-Host "Creating N/W rule from app to business tier"
-    $subnetAddressPrefix = ((Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $VnetName).Subnets | Where-Object { $_.Name -eq $BackEndsubnetName }).AddressPrefix
-    $loadbalancerFEIP = (Get-AzLoadBalancer -Name $ILBName -ResourceGroupName $ResourceGroupName).FrontendIpConfigurations[0].PrivateIpAddress
-
-    $nsgCreateParams = @{
-
-        RuleName                 = $NWRuleName
-        RuleDescription          = $NWRuleDescription
-        Protocol                 = $NWProtocol
-        SourcePortRange          = $SourcePortRange
-        DestinationPortRange     = $DestinationPortRange
-        SourceAddressPrefix      = $subnetAddressPrefix
-        DestinationAddressPrefix = $loadbalancerFEIP
-        Access                   = 'Allow'
-        Priority                 = '120'
-        Direction                = 'Inbound'
-        ResourceGroupName        = $ResourceGroupName
-        NSGName                  = $NSGName
-        Location                 = $Location
-        TaggingData              = $TaggingData
-    }
-
-
-    New-NetworkSecurityGrp @nsgCreateParams
-
-    Write-Host "Created N/W rule from app to business tier"
 
     #endRegion
 
